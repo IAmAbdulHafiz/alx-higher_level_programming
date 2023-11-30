b80ff2dbfd6442453fde74c104ca06da24464f29
IAmAbdulHafiz <abdulhafiz.yussif@aiesec.net>
2023-11-29 21:45:05 -0800

    Updated some files

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
-    return 'a' <= c <= 'z'
-
-if __name__ == "__main__":
-    islower_test_cases = ["a", "H", "A", "3", "g"]
-    for char in islower_test_cases:
-        print("{} is {}".format(char, "lower" if islower(char) else "upper"))
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
