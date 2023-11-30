97234832f44842da1b73f66d2700a62060e855d1
IAmAbdulHafiz <abdulhafiz.yussif@aiesec.net>
2023-11-29 21:48:33 -0800

    updated a file

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 
-def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print(last_digit, end='')
-    return last_digit
-
-if __name__ == "__main__":
-    print_last_digit(98)
-    print_last_digit(0)
-    r = print_last_digit(-1024)
-    print(r)
+def print_last_digit(num):
+    print(abs(num) % 10, end="")
+    return (abs(num) % 10)
