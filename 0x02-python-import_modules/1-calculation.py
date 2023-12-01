3fa19ff8c932c649086e0347a61f911753e4988f
IAmAbdulHafiz <abdulhafiz.yussif@aiesec.net>
2023-11-30 20:54:18 -0800

    Updated 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-res_add = add(a, b)
-res_sub = sub(a, b)
-res_mul = mul(a, b)
-res_div = div(a, b)
+    res_add = add(a, b)
+    res_sub = sub(a, b)
+    res_mul = mul(a, b)
+    res_div = div(a, b)
 
-print("{:d} + {:d} = {:d}".format(a, b, res_add))
-print("{:d} - {:d} = {:d}".format(a, b, res_sub))
-print("{:d} * {:d} = {:d}".format(a, b, reis_mul))
-print("{:d} / {:d} = {:d}".format(a, b, res_div))
+    print("{} + {} = {}".format(a, b, res_add))
+    print("{} - {} = {}".format(a, b, res_sub))
+    print("{} * {} = {}".format(a, b, res_mul))
+    print("{} / {} = {}".format(a, b, res_div))
