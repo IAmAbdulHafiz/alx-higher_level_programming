008e16a746f80642d90e27537f873b06c943415a
IAmAbdulHafiz <abdulhafiz.yussif@aiesec.net>
2023-11-30 21:17:10 -0800

    Updated

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,5 +7,3 @@ if __name__ == "__main__":
     for i in range(len(sys.argv) - 1):
         totalNumber += int(sys.argv[i + 1])
     print("{}".format(totalNumber))
-
-# Abdu-Hafiz Yussif <abdulhafiz.yussif@aiesec.net>
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+
+    _names = dir(hidden_4)
+    for _name in _names:
+        if _name[:2] != "__":
+            print(_name)
