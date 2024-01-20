ca77e60705e681e9985adde145caf8615dd69e2f
Abdul-Hafiz Yussif <abdulhafiz.yussif@aiesec.net>
2024-01-20 02:10:06 +0000

    Updated some files tests directory

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def magic_string():
+    """
+    Generates a string with "Holberton" based on function invocation count.
+    """
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return ("Holberton, " * (magic_string.n - 1) + "Holberton")
