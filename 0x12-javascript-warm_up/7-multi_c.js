df387f7d5ffd64a95629cc90fb4b2585dd217cf2
IAmAbdulHafiz <abdulhafiz.yussif@aiesec.net>
2024-03-20 04:16:16 -0700

    Added task 4 - 17

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
-  }
-}
+const x = parseInt(process.argv[2]);
 
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  let output = "";
+  for (let i = 0; i < x; i++) {
+    output += "C is fun\n";
+  }
+  console.log(output.trim());
+}
