14b0bba41c79c13cd3e31b383d0ee4792fe9a81e
IAmAbdulHafiz <abdulhafiz.yussif@aiesec.net>
2024-04-23 18:33:30 -0700

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const endPoint = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request({ url: endPoint, methods: 'GET' }, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(body && JSON.parse(body).title);
+  }
 });
